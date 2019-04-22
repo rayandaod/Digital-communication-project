3e9772d147c4989b123c2682c24d2fe55c498258
Rayan DAOD <rayan.daodnathoo@epfl.ch>
2019-04-22 17:18:02 +0000

    Correct mappings to work with all M for PSK

--- mappings.py
+++ mappings.py
@@ -1,23 +1,22 @@
 import numpy as np
 
 
+# Returns a M-QAM constellation
 def qam_map(M):
-    check_mappings(M)
+    log_sqrt_m = np.log2(np.sqrt(M))
+    if log_sqrt_m != np.ceil(log_sqrt_m):
+        raise ValueError('Parameter[M] is not of the form 2^2K, K a positive integer.')
     N = np.sqrt(M) - 1
     aux = np.arange(-N, N+1, 2)
     x, y = np.meshgrid(aux, aux[::-1])
     return x + 1j*y
 
+# Returns a M-PSK constellation
 def psk_map(M):
-    check_mappings(M)
     return np.exp(1j*2*np.pi*np.arange(0, M)/M)
 
-def check_mappings(M):
-    log_sqrt_m = np.log2(np.sqrt(M))
-    if log_sqrt_m != np.ceil(log_sqrt_m):
-        raise ValueError('Parameter[M] is not of the form 2^2K, K a positive integer.')
 
 if __name__ == '__main__':
-    M = 16
+    M = 3
     print(qam_map(M))
     print(psk_map(M))
