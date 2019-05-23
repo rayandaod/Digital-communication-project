7c08e9aee79eda4aa9ba08613c167af8e550e979
Rayan DAOD <rayan.daodnathoo@epfl.ch>
2019-05-23 12:29:54 +0200

    Local tests in the test.py file work (with and without mod/demod)

--- src/test.py
+++ src/test.py
@@ -7,13 +7,19 @@ import synchronization
 import params
 import transmitter
 import receiver
+import fourier_helper
 
 
 """
 Testing file
 """
-if __name__ == "__main__":
-    print(params.T*params.Fs)
+
+
+def local_test_without_modulation():
+    """
+       Test the design locally without modulation nor demodulation
+       :return: None
+    """
     ints = transmitter.message_to_ints()
     symbols = transmitter.encoder(ints, mappings.mapping)
 
@@ -54,11 +60,11 @@ if __name__ == "__main__":
     plot_helper.plot_complex_function(y, "y in Time domain")
     plot_helper.fft_plot(y, "y in Frequency domain", shift=True)
 
-    data_samples = y[delay + len(h)-1:len(y)-len(h)+1]
+    data_samples = y[delay + len(h) - 1:len(y) - len(h) + 1]
     plot_helper.plot_complex_function(data_samples, "y after puting the right sampling time")
 
     symbols_received = data_samples[::params.USF]
-    print("Symbols received:\n{}",format(symbols_received))
+    print("Symbols received:\n{}", format(symbols_received))
 
     data_symbols = symbols_received[len(preamble):]
 
@@ -67,3 +73,101 @@ if __name__ == "__main__":
 
     ints = receiver.decoder(data_symbols, mappings.mapping)
     receiver.ints_to_message(ints)
+
+
+def local_test():
+    """
+    Test the design locally with modulation and the demodulation
+    :return: None
+    """
+    ints = transmitter.message_to_ints()
+    symbols = transmitter.encoder(ints, mappings.mapping)
+
+    # Generate the pulse h
+    _, h = pulses.root_raised_cosine()
+
+    # Generate the preamble symbols and the preamble (shaped) samples
+    preamble = synchronization.generate_sync_sequence(len(symbols))
+    preamble_samples = upfirdn(h, preamble, params.USF)
+
+    # Stick the preamble symbols with the data symbols
+    total_symbols = np.concatenate((preamble, symbols))
+
+    # Shape the signal with the pulse h
+    total_samples = upfirdn(h, total_symbols, params.USF)
+
+    print("Shaping the preamble...")
+    print("Number of symbols for the preamble: {}".format(len(preamble)))
+    print("Number of samples for the preamble: {}".format(len(preamble_samples)))
+    print("--------------------------------------------------------")
+    plot_helper.plot_complex_function(preamble_samples, "Synchronization sequence shaped, in Time domain")
+    plot_helper.fft_plot(preamble_samples, "Synchronization sequence shaped, in Frequency domain", shift=True)
+
+    print("Shaping the preamble and the data...")
+    print("Shaping the data...")
+    print("Up-sampling factor: {}".format(params.USF))
+    print("Number of samples: {}".format(len(total_samples)))
+    print("Minimum sample: {}".format(min(total_samples)))
+    print("Maximum sample: {}".format(max(total_samples)))
+    print("--------------------------------------------------------")
+    plot_helper.plot_complex_function(total_samples, "Total samples in Time domain")
+    plot_helper.fft_plot(total_samples, "Total samples in Frequency domain", shift=True)
+
+    # Modulate the total_samples
+    if params.MODULATION_TYPE == 1:
+        samples = fourier_helper.modulate(total_samples, params.np.mean(params.FREQ_RANGES, axis=1))
+    elif params.MODULATION_TYPE == 2:
+        samples = fourier_helper.modulate(total_samples, [params.FREQ_RANGES[0][1], params.FREQ_RANGES[2][1]])
+    else:
+        raise ValueError('This modulation type does not exist yet... Hehehe')
+
+    maximum = max(samples)
+
+    if params.verbose:
+        print("Modulation of the signal...")
+        print("Number of samples: {}".format(len(samples)))
+        print("Minimum sample after modulation: {}".format(min(samples)))
+        print("Maximum sample after modulation: {}".format(maximum))
+        print("--------------------------------------------------------")
+        plot_helper.plot_complex_function(samples, "Input samples after modulation, in Time domain")
+        plot_helper.fft_plot(samples, "Input samples after modulation, in Frequency domain", shift=True)
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # Channel simulation
+    # ----------------------------------------------------------------------------------------------------------------
+
+    # Demodulate the samples with the appropriate frequency fc
+    demodulated_samples = fourier_helper.demodulate(samples, 2000)
+
+    # Find the delay
+    delay = synchronization.maximum_likelihood_sync(demodulated_samples, synchronization_sequence=preamble_samples)
+    print("Delay: {} samples".format(delay))
+    print("--------------------------------------------------------")
+
+    # Match filter
+    h_matched = np.conjugate(h[::-1])
+    y = np.convolve(total_samples, h_matched)
+    plot_helper.plot_complex_function(y, "y in Time domain")
+    plot_helper.fft_plot(y, "y in Frequency domain", shift=True)
+
+    # Crop the samples (remove the ramp-up and ramp-down)
+    data_samples = y[delay + len(h) - 1:len(y) - len(h) + 1]
+    plot_helper.plot_complex_function(data_samples, "y after puting the right sampling time")
+
+    # Down-sample
+    symbols_received = data_samples[::params.USF]
+    print("Symbols received:\n{}", format(symbols_received))
+
+    # Remove the preamble symbols
+    data_symbols = symbols_received[len(preamble):]
+
+    plot_helper.plot_complex_function(data_symbols, "y after puting the right sampling time")
+    plot_helper.plot_complex_symbols(data_symbols, "Symbols received", annotate=False)
+
+    # Decode the symbols
+    ints = receiver.decoder(data_symbols, mappings.mapping)
+    receiver.ints_to_message(ints)
+
+
+if __name__ == "__main__":
+    local_test()
